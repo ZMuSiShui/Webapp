9de38f6b78b13f8dc2c8f9570542f10dbe293582
zhang <zhangjieepic@gmail.com>
2021-07-31 00:10:19 +0800

    修改为简约风

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
-<template>
-  <div>
-    <full-page ref="fullpage" :options="options" id="fullpage">
-      <div class="section">
-        <Welcome />
-      </div>
-      <div class="section">
-        <Winnav />
-      </div>
-      <!-- <div class="section">
-        <h2>Second 3</h2>
-      </div> -->
-    </full-page>
-  </div>
+<template class="app">
+  <Welcome style="z-index: -1" />
 </template>
 
 <script>
 import Welcome from "./components/Welcome.vue";
-import Winnav from "./components/Winnav.vue";
 
 export default {
   name: "App",
   components: {
     Welcome,
-    Winnav,
   },
   data() {
     return {
       leaveTitle: "ヾ(￣▽￣)Bye~Bye~",
       returnTitle: "~(～￣▽￣)～",
-      options: {
-        menu: "#menu",
-        navigation: true,
-        afterLoad: this.afterLoad,
-        anchors: ["home", "nav"],
-      },
     };
   },
   mounted() {
@@ -56,33 +36,17 @@ export default {
         }
       });
     },
-    afterLoad() {
-      /* eslint-disable */
-      $(".tiles-group").animate({
-        left: 0,
-      });
-
-      $(window).on(Metro.events.resize + "-start-screen-resize", function () {
-        setTilesAreaSize();
-      });
-
-      $(window).on(Metro.events.mousewheel, function (e) {
-        var up = e.deltaY < 0 ? -1 : 1;
-        var scrollStep = 50;
-        $(".start-screen")[0].scrollLeft += scrollStep * up;
-      });
-    },
   },
 };
 </script>
-<style>
-@import url(https://unpkg.com/fullpage.js/dist/fullpage.min.css);
-* {
+
+<style >
+html,
+body,
+div {
+  height: 100%;
+  width: 100%;
   margin: 0;
   padding: 0;
-}
-.section {
-  text-align: center;
-  color: aliceblue;
 }
 </style>
