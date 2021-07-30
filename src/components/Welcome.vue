e01cc4509ea3232a08639e88b711e7743decf3c5
zhang <zhangjieepic@gmail.com>
2021-07-31 00:10:05 +0800

    修改为简约风

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,42 +1,60 @@
 <template>
-  <div id="wrapper">
-    <section id="main">
-      <header>
-        <span class="avatar">
-          <a href="https://blog.zhangjie.me">
-            <img src="../assets/images/logo.png" width="118px" height="118px" />
+  <div>
+    <div id="wrapper">
+      <section id="main">
+        <header>
+          <span class="avatar">
+            <a href="https://blog.musishui.net">
+              <img
+                src="../assets/images/logo.png"
+                width="118px"
+                height="118px"
+              />
+            </a>
+          </span>
+          <a href="https://blog.musishui.net">
+            <h1 style="color: #ffffff">MuSiShui</h1>
           </a>
-        </span>
-        <a href="https://blog.zhangjie.me">
-          <h1 style="color: #ffffff">MuSiShui</h1>
-        </a>
-        <div class="social">
-          <a target="_blank" href="mailto:zhangjieepic@gmail.com" class="mail">
-            <i class="icon-mail"></i>
-          </a>
-          <a target="_blank" href="https://github.com/ZMuSiShui" class="github">
-            <i class="icon-github"></i>
-          </a>
-          <a
-            target="_blank"
-            href="https://twitter.com/ZMuSiShui"
-            class="twitter"
-          >
-            <i class="icon-twitter"></i>
-          </a>
-        </div>
-        <p style="margin-top: 10px">七情拆解，无非心印；六欲所指，皆由心生。</p>
-      </header>
-      <footer id="footer">
-        <ul class="copyright">
-          <li>© since 2021</li>
-          <li>
-            by
-            <a href="https://github.com/ZMuSiShui" style="color: #ffffff">MuSiShui</a>
-          </li>
-        </ul>
-      </footer>
-    </section>
+          <div class="social">
+            <a
+              target="_blank"
+              href="mailto:zhangjieepic@gmail.com"
+              class="mail"
+            >
+              <i class="icon-mail"></i>
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/ZMuSiShui"
+              class="github"
+            >
+              <i class="icon-github"></i>
+            </a>
+            <a
+              target="_blank"
+              href="https://twitter.com/ZMuSiShui"
+              class="twitter"
+            >
+              <i class="icon-twitter"></i>
+            </a>
+          </div>
+          <p style="margin-top: 10px">
+            七情拆解，无非心印；六欲所指，皆由心生。
+          </p>
+        </header>
+        <footer id="footer">
+          <ul class="copyright">
+            <li>© since 2021</li>
+            <li>
+              by
+              <a href="https://github.com/ZMuSiShui" style="color: #ffffff"
+                >MuSiShui</a
+              >
+            </li>
+          </ul>
+        </footer>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -45,4 +63,5 @@ export default {
   name: "Welcome",
 };
 </script>
-<style src="../assets/style/Welcome.css" scoped></style>
+<style src="../assets/style/index.css"></style>
+<style src="../assets/style/social.css"></style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Metro from 'metro4'
-import './plugins/fullpage.js'
+import '@/assets/sakura'
 
-
-Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-  mounted: function () {
-      Metro.init();
-  }
+  render: h => h(App)
 }).$mount('#app')
